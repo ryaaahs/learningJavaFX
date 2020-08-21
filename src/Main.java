7e19007192c01f0c7482a8185f2821579f63a80a
Reily Maahs <reily.maahs@gmail.com>
2020-08-21 14:25:49 -0400

    First commit for the project, Learning about javaFX.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("hello");
+        Application.launch(javaApplication.class, args);
+    }
+}
---
+++ src/javaApplication.java
@@ -0,0 +1,72 @@
+import javafx.application.Application;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class javaApplication extends Application {
+
+    /*
+    @Override
+    public void init() throws Exception {
+        System.out.println("Start");
+        // Loading assets or pictures
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("End");
+    }
+    */
+
+    /*@Override
+    public void start(Stage stage) throws Exception {
+        Stage stage2 = new Stage();
+        stage.setTitle("First Window");
+
+        stage.show();
+
+
+
+        stage2.setWidth(400);
+        stage2.setHeight(300);
+
+
+        //Allows for the window to be in the center of the screen
+        int screenHalfWidth = 1920 / 2;
+        int screenHalfHeight = 1080 / 2;
+        int winHalfWidth = (int)(stage2.getWidth() / 2);
+        int winHalfHeight = (int)(stage2.getHeight() / 2);
+
+
+
+        stage2.setX(screenHalfWidth - winHalfWidth);
+        stage2.setY(screenHalfHeight - winHalfHeight);
+
+
+        stage2.setTitle("Second Window");
+        stage2.show();
+
+        //Modality
+        //How they interact with each other
+
+        Stage stage3 = new Stage();
+        stage3.initStyle(StageStyle.UNIFIED);
+        stage3.setTitle("Third Window");
+        stage3.show();
+
+        stage.toFront();
+        */
+
+        @Override
+        public void start(Stage stage) throws Exception {
+            
+        }
+    }
+}
